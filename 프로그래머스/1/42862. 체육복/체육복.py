b01511b09c934d156f296577cee21596507b024b
bykbyk0401 <102952855+bykbyk0401@users.noreply.github.com>
2026-08-22 17:55:53 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,22 +1,14 @@
 def solution(n, lost, reserve):
-    status = [1]*(n+2)
-    for i in lost :
-        status[i] -= 1
-    for j in reserve :
-        status[j] += 1
-    for k in range(n) :
-        if status[k+1] == 0 :
-            if status[k] == 2 and status[k+2] == 2 :
-                status[k+1] += 1
-                status[k] -= 1
-            elif status[k] == 2 :
-                status[k+1] += 1
-                status[k] -= 1
-            elif status[k+2] == 2 :
-                status[k+1] += 1
-                status[k+2] -= 1
-    answer = -2
-    for i in status :
-        if i > 0 :
-            answer += 1
+    real_reserve = set(reserve) - set(lost)
+    real_lost = set(lost) - set(reserve)
+    
+    answer = n - len(real_lost)
+    
+    for i in sorted(real_lost):
+        if int(i)-1 in real_reserve:
+            real_reserve.remove(i-1)
+            answer+=1
+        elif int(i)+1 in real_reserve:
+            real_reserve.remove(i+1)
+            answer+=1
     return answer
